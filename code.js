248674991087182bdda0678663acd9a13b8799a1
bueast <billyrobertschef@gmail.com>
2023-01-15 11:41:32 +1300

    First function done. 2nd is a mess

---
+++ code.js
@@ -0,0 +1,26 @@
+function getComputerChoice() {
+    let x = Math.floor((Math.random() * 3) + 1);
+
+    if (x == 1) {
+        console.log('Rock')
+    }
+    else if (x == 2) {
+        console.log('paper')
+    }
+    else if (x == 3) {
+        console.log('Scissors')
+    }
+}
+  
+function playerChoice() {
+    let y = 
+    if (y = paper) {
+        console.log('Player Paper')
+    }
+    else if (y = scissors) {
+        console.log('Player Scissors')
+    }
+    else if (y = rock) {
+        console.log('Player Rock')
+    }
+}
