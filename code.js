b73ee01a6b9991eec101f966aec307f78e29060a
bueast <billyrobertschef@gmail.com>
2023-01-19 06:40:58 +1300

    updated to work out bug

--- code.js
+++ code.js
@@ -1,26 +1,60 @@
-function getComputerChoice() {
-    let x = Math.floor((Math.random() * 3) + 1);
+const computerChoice = getComputerChoice();
+const playerChoice = getPlayerChoice();
 
-    if (x == 1) {
-        console.log('Rock')
+function getComputerChoice() {
+    let randomNumber = Math.floor((Math.random() * 3) + 1);
+
+    if (randomNumber == 1) {
+        return 'Rock'
     }
-    else if (x == 2) {
-        console.log('paper')
+    else if (randomNumber == 2) {
+        return 'paper'
     }
-    else if (x == 3) {
-        console.log('Scissors')
+    else if (randomNumber == 3) {
+        return 'Scissors'
     }
 }
-  
-function playerChoice() {
-    let y = 
-    if (y = paper) {
-        console.log('Player Paper')
+
+function getPlayerChoice() {
+    let y = document.getElementsByClassName("choice".); // This is the part I need help connecting //
+
+    if (y = 'paper') {
+        return 'player paper'
     }
-    else if (y = scissors) {
-        console.log('Player Scissors')
+    else if (y = 'scissors') {
+        return 'player Scissors'
     }
-    else if (y = rock) {
-        console.log('Player Rock')
+    else if (y = 'rock') {
+        return 'player rock'
+    }
+}
+
+function playGame(playerChoice, computerChoice) {
+    if (playerChoice === 'rock' && computerChoice === 'rock') {
+        return 'It is a draw!'
+    }
+    else if (playerChoice === 'rock' && computerChoice === 'paper') {
+        return 'You lose!'
+    }
+    else if (playerChoice === 'rock' && computerChoice === 'scissors') {
+        return 'You win!!!'
+    }
+    else if (playerChoice === 'paper' && computerChoice === 'paper') {
+        return 'It is a draw!'
+    }
+    else if (playerChoice === 'paper' && computerChoice === 'scissors') {
+        return 'You lose!'
+    }
+    else if (playerChoice === 'paper' && computerChoice === 'rock') {
+        return 'You win!!!'
+    }
+    else if (playerChoice === 'scissors' && computerChoice === 'scissors') {
+        return 'It is a draw!'
+    }
+    else if (playerChoice === 'scissors' && computerChoice === 'rock') {
+        return 'You lose!'
+    }
+    else if (playerChoice === 'scissors' && computerChoice === 'paper') {
+        return 'You win!!!'
     }
 }
